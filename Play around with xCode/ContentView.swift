b60b27cf6f501c608e174d2d3be8dcfa96378761
IsackHaer <isack.h@yahoo.com>
2023-04-24 12:35:51 +0200

    Initial commit

--- Play around with xCode/ContentView.swift
+++ Play around with xCode/ContentView.swift
@@ -9,13 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color("arsenal").ignoresSafeArea()
+            
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, world!")
+                Text("Swift testing")
+                Text("Title Large")
+                    .font(.largeTitle)
+                Text("Title 1")
+                    .font(.title)
+                Text("Title 2")
+                    .font(.title2)
+                Text("Title 3")
+                    .font(.title3)
+                Text("Headline")
+                    .font(.headline)
+                Text("Body")
+                    .font(.custom("Bradley Hand", size: 20, relativeTo: .headline))
+                Image("swiftIcon")
+                    .resizable(resizingMode: .stretch)
+                    .frame(width: 100, height: 100)
+            }
         }
-        .padding()
     }
 }
 
