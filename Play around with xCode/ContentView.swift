45266d1ff89a55382cecd7500859e9c2a3005854
IsackHaer <isack.h@yahoo.com>
2023-04-24 10:14:50 +0200

    Initial Commit

---
+++ Play around with xCode/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Play around with xCode
+//
+//  Created by Isack Häring on 24.04.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Play around with xCode/Play_around_with_xCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Play_around_with_xCodeApp.swift
+//  Play around with xCode
+//
+//  Created by Isack Häring on 24.04.23.
+//
+
+import SwiftUI
+
+@main
+struct Play_around_with_xCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
